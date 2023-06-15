e6ff6bfd2200438914b1bed30a3f337f1eb448c6
Ulugbek laptop <ulugb3k270@gmail.com>
2023-06-15 20:31:27 +0500

    bugfix: console errors resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <ReactQueryProvider>
-        <body className={inter.className}>{children}</body>
-      </ReactQueryProvider>
+      <body className={inter.className}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
